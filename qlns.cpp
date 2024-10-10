7521d001fb5340c21f3537b5a129cf9cb47ac3f8
thien05 <117966951+Thien262005@users.noreply.github.com>
2024-10-10 13:48:04 +0700

    Add files via upload

---
+++ qlns.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+template <typename T>
+class Node {
+private:
+    T user;
+    Node* nextNode;
+
+public:
+    Node() : user(T()), nextNode(nullptr) {}
+    Node(T user) : user(user), nextNode(nullptr) {}
+    T getDanhSach() {
+        return user;
+    }
+    Node* getNext() {
+        return nextNode;
+    }
+    void setNext(Node* next) {
+        nextNode = next;
+    }    
+    void setNode(T newValue) {
+        user = newValue;
+    }
+};
+int main() {
+    Node<int> node;
+}
+
