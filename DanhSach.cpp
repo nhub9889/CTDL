e759efc8759a0680c46bec54c796d0a18241ef8c
nhub9889 <153304029+nhub9889@users.noreply.github.com>
2024-10-11 01:12:30 +0700

    Update DanhSach.cpp

--- DanhSach.cpp
+++ DanhSach.cpp
@@ -1,77 +1,18 @@
 #include <iostream>
 using namespace std;
-class Info {
-    public:
-        string ten, dienThoai, diaChi, email;
 
-    public:
-        Info(string _ten, string _dienthoai, string _diachi, string _email) {
-            ten = _ten;
-            dienThoai = _dienthoai;
-            diaChi = _diachi;
-            email = _email;
-        }
-        string getTen() {
-            return ten;
-        }
-
-        void setTen(string _ten) {
-            ten = _ten;
-        }
-
-        string getDienThoai() {
-            return dienThoai;
-        }
-
-        void setDienThoai(string _dienThoai) {
-            dienThoai = _dienThoai;
-        }
-
-        string getDiaChi() {
-            return diaChi;
-        
-
-        void setDiaChi(string _diaChi) {
-            diaChi = _diaChi;
-        }
-
-        string getEmail() {
-            return email;
-        }
-
-        void setEmail(string _email) {
-            email = _email;
-        }
-};
-
-class Employee {
-    public:
-        string username, password;
-
-    public:
-        Employee(string _user, string _pass) {
-            username = _user;
-            password = _pass;
-        }
-        void XemThongTin() {
-        
-        }
-
-        void DoiPassWord(string newPassword) {
-            password = newPassword;
-        }
-};
+template <class T>
 class Danh_Sach {
     private:
         int SoLuong;
-    private:
+        Node <T> Dau;
+        Node <T> Cuoi;
+    public:
         void DocDanhSach();
         void XayDanhSach(user);
         void HuyDanhSach();
         void Them(T);
         void Xoa(T);
         void CapNhat(T);
+        void DangNhap();
 };
-int main() {
-    return 0;
-}
