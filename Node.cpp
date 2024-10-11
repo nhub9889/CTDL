ce4cb03b4100a226e360bc07ab47c19bdda82a25
nhub9889 <153304029+nhub9889@users.noreply.github.com>
2024-10-11 13:27:27 +0700

    Create Node.cpp

---
+++ Node.cpp
@@ -0,0 +1,9 @@
+Info thongtin;
+    public :
+    string getUsername() {return username;}
+    string getPassword() {return password;}
+    Info getThongTin() {return thongtin;}
+    void setUsername(string _username) { username = _username;}
+    void setPassword (string _password) {password = _password;}
+    setThongTin(Info _thongtin) { thongtin = _thongtin;}
+    void Thoat() {return EXIT_SUCCESS;}
