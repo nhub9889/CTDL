3a1477f5c1db09ec933a1254dee147b5e44db7a4
nhub9889 <153304029+nhub9889@users.noreply.github.com>
2024-10-11 13:26:42 +0700

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -38,17 +38,32 @@ class Info{
 
 };
 class Employee{
-    public :
+    private :
     string username, password;
+    Info thongtin;
     public :
+    string getUsername() {return username;}
+    string getPassword() {return password;}
+    Info getThongTin() {return thongtin;}
+    void setUsername(string _username) { username = _username;}
+    void setPassword (string _password) {password = _password;}
+    setThongTin(Info _thongtin) { thongtin = _thongtin;}
+    void Thoat() {return EXIT_SUCCESS;}
     void XemThongTin();
     void DoiPassWord(string);
 
 };
 class Admin{
-    public:
-    string username, password;
+    private:
+    Info thongtin;
     public :
+    string getUsername() {return username;}
+    string getPassword() {return password;}
+    Info getThongTin() {return thongtin;}
+    void setUsername(string _username) { username = _username;}
+    void setPassword (string _password) {password = _password;}
+    setThongTin(Info _thongtin) { thongtin = _thongtin;}
+    void Thoat() {return EXIT_SUCCESS;}
     void ThemEmployee(Employee);
     bool TimEmployee(Info);
     void XoaEmployee(string, bool);
