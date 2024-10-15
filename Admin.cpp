def39341386c32a58330a7d66e6320a9d8117cab
nhub9889 <153304029+nhub9889@users.noreply.github.com>
2024-10-16 01:14:18 +0700

    Update Admin.cpp

--- Admin.cpp
+++ Admin.cpp
@@ -13,10 +13,37 @@ class Admin{
     void setPassword (string _password) {password = _password;}
     void setThongTin(Info _thongtin) { thongtin = _thongtin;}
 
-    Node* timNhanVien(string username) {
-        return danhSach.timNhanVien(username);
+    Node* timEmployee(string user) {
+        return danhSachEmployee.tim(user);
+    }
+
+    bool KiemTraMatKhau(string pass){
+        return pass == password;
+    }
+
+    void CapNhatEmployee(){
+        
     }
     
+    Info hienThiEmployee(string user) {
+        Node* nodeNhanVien = danhSachEmployee.tim(user);
+        if (nodeNhanVien != nullptr) {
+            return nodeNhanVien->Employee->getThongTin();
+        }
+        return nullptr;  
+    }
+    
+    bool ThemEmployee(string user){
+        if (!timEmployee(user)==nullptr){
+            DanhSachEmployee.Them(emp);
+            return true;
+        }
+        return false;
+    }
+    
+    void XoaEmployee(){
+        danhSachEmployee.Xoa();
+    }
     void Thoat() {return EXIT_SUCCESS;}
     
 
